02da7ae7820005db96cf140b02033893f405ef19
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-05-23 15:49:10 +0300

    feat: add fetching data from rss url
    rus: добавляет получение данных по rss url

--- src/application.js
+++ src/application.js
@@ -1,4 +1,5 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 import * as yup from 'yup';
 
 const validateUrl = (url) => {
@@ -11,8 +12,18 @@ const validateUrl = (url) => {
   }
 };
 
+const queryRss = async (url) => {
+  const response = await axios.get(url);
+  return response.data;
+};
+
+const avoidCorsProblem = (url) => (
+  `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`
+);
+
 export default () => {
   const state = {
+    error: null,
     rssForm: {
       fields: {
         url: {
@@ -21,6 +32,7 @@ export default () => {
           value: '',
         },
       },
+      status: 'filling',
     },
   };
 
@@ -28,7 +40,7 @@ export default () => {
   const form = document.querySelector('.rss-form');
   input.focus();
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const formData = new FormData(e.target);
@@ -49,6 +61,23 @@ export default () => {
       value: rssUrl,
     };
 
+    const urlWithoutCorsProblem = avoidCorsProblem(rssUrl);
+
+    try {
+      // state.error = null;
+      // state.form.status = 'loading';
+      console.log(rssUrl);
+      const todo = await queryRss(urlWithoutCorsProblem);
+      console.log(todo);
+      // state.form.status = 'filling';
+      // state.todos.push(todo);
+    } catch (err) {
+      console.log(err);
+      // state.form.status = 'failed';
+      // state.error = err.message;
+    }
+
+    form.reset();
     input.focus();
   });
 };
