ebe349e5c0a374042a225f7824bf0899351f125f
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-06-04 16:10:20 +0300

    fix: error with site without rss feed
    rus: исправляет ошибку связанную с сайтом без rss

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -5,6 +5,7 @@ export default {
       invalidUrl: 'Ссылка должна быть валидным URL',
       isAdded: 'RSS уже подключен',
       network: 'Ошибка сети',
+      withoutRss: 'Ресурс не содержит валидный RSS',
     },
     success: 'RSS успешно загружен',
     posts: {
